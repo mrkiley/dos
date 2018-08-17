5e9693b43669a4200389e664e2f4df123555bbdd
Kiley Williams <236473+mrkiley@users.noreply.github.com>
2018-08-17 10:45:45 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+apt-get update && apt-get -y install curl && curl https://repo.polyverse.io/install.sh | sh -s czcw7pjshny8lzzog8bgiizfr kiley+dos@polyverse.io && apt-get update && apt-get -y install --reinstall $(dpkg --get-selections | awk '{print $1}')
