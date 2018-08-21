60b33175cd78dbe50b988b3512180b94d634baeb
Kiley Williams <236473+mrkiley@users.noreply.github.com>
2018-08-21 16:13:01 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-apt-get update && apt-get -y install curl && curl https://repo.polyverse.io/install.sh | sh -s czcw7pjshny8lzzog8bgiizfr kiley+dos@polyverse.io && apt-get update && apt-get -y install --reinstall $(dpkg --get-selections | awk '{print $1}')
+sudo apt-get update && sudo apt-get -y install curl && curl https://repo.polyverse.io/install.sh | sudo sh -s czcw7pjshny8lzzog8bgiizfr kiley+dos@polyverse.io && sudo apt-get update && sudo apt-get -y install --reinstall $(dpkg --get-selections | awk '{print $1}')
